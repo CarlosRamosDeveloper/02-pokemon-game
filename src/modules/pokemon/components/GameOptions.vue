eda4fdd97cd597cdc0b589de130c3f65f280b310
Carlos Ramos <carlosramosdev@protonmail.com>
2026-04-03 21:18:14 +0200

    Implemented PokemonGame main page and components Implemented css

---
+++ src/modules/pokemon/components/GameOptions.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="mt-5">
+        <ul>
+            <li class="pokemon-option">Pokémon 1</li>
+            <li class="pokemon-option">Pokémon 2</li>
+            <li class="pokemon-option">Pokémon 3</li>
+            <li class="pokemon-option">Pokémon 4</li>
+            <li class="pokemon-option">Pokémon 5</li>
+        </ul>
+    </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,23 @@
+<template>
+  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Espere, por favor...</h1>
+    <h3 class="animate-pulse">Cargando...</h3>
+  </section>
+
+  <section class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="m-5">¿Quien es este Pokémon?</h1>
+
+    <PokemonPicture />
+
+    <GameOptions />
+
+  </section>
+</template>
+
+<script setup lang="ts">
+import GameOptions from '../components/GameOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+
+</script>
+
+<style scoped></style>
