6e2c4b2d16fa4cd3b2d69ab11a76c9a1d9c23c2a
Carlos Ramos <carlosramosdev@protonmail.com>
2026-04-04 09:38:44 +0200

    Implemented loading stage at PokemonGame page

--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,10 +1,10 @@
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl">Espere, por favor...</h1>
     <h3 class="animate-pulse">Cargando...</h3>
   </section>
 
-  <section class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">¿Quien es este Pokémon?</h1>
 
     <PokemonPicture />
@@ -19,7 +19,7 @@ import GameOptions from '../components/GameOptions.vue';
 import PokemonPicture from '../components/PokemonPicture.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
 
-const { gameStatus } = usePokemonGame()
+const { gameStatus, isLoading } = usePokemonGame()
 
 console.log(gameStatus)
 
